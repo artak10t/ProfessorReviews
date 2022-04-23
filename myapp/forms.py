64723e21cee9dcbe40a1416c9e0b3171bfafb942
Spartak <artak10t@gmail.com>
2022-04-23 11:38:42 -0700

    homepage updated

--- myapp/forms.py
+++ myapp/forms.py
@@ -3,18 +3,18 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextA
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()])
+    email = StringField('SJSU Email', validators=[DataRequired()])
     password = PasswordField('Password')
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Sign in')
 
 class RegisterForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()])
+    email = StringField('SJSU Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     repeatPassword = PasswordField('Repeat Password', validators=[DataRequired()])
     submit = SubmitField('Register')
 
 class GrantModeratorForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()])
+    email = StringField('SJSU Email', validators=[DataRequired()])
     status = BooleanField('Moderator')
     submit = SubmitField('Grant')
